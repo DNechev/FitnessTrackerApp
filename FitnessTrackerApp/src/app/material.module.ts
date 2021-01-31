eb8c4683960e6d06b9de86a40a76f294e334787b
DNechev <49711661+DNechev@users.noreply.github.com>
2021-01-31 22:23:48 +0100

    Signup form
    Adding a date picker

--- FitnessTrackerApp/src/app/material.module.ts
+++ FitnessTrackerApp/src/app/material.module.ts
@@ -3,6 +3,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+
 
 
 @NgModule({
@@ -10,13 +13,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialModule{
